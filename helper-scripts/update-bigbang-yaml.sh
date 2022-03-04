fd6d284a5bd7eea5d9e7216a1d095072b99691bf
Tim Meyers <metnem@gmail.com>
2022-03-04 11:13:37 -0500

    helper script - first pass - not used

---
+++ helper-scripts/update-bigbang-yaml.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Takes 3 arguments
+# $1 = environment, e.g. dev or prod
+# $2 = github repository
+# $3 = branch name
+
+#echo $1 $2 $3
+
+### URL update
+
+URL="http://github.com/$2"
+ESC_URL=$(printf '%s\n' "$URL" | sed -e 's/[\/&]/\\&/g')
+
+sed -i $1/bigbang.yaml -e "s/__CHANGE_ME_REPO_URL__/${ESC_URL}/g"
+
+## Branch update
+
+BRANCH=$3
+ESC_BRANCH=$(printf '%s\n' "$BRANCH" | sed -e 's/[\/&]/\\&/g')
+
+sed -i $1/bigbang.yaml -e "s/__CHANGE_ME_REPO_BRANCH__/${ESC_BRANCH}/g"
