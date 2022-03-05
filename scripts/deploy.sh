4cc05c2a6439cb96dc1da0c352bcee4023d59714
Tim Meyers <metnem@gmail.com>
2022-03-05 15:48:27 -0500

    change deploy sleep from 15 to 60s

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -137,7 +137,7 @@ kubectl apply -f bigbang.yaml
 popd
 
 echo -e "\n\e[36m###\e[33m 💤 Sleeping for a few seconds...\e[39m"
-sleep 15
+sleep 60
 
 echo -e "\n\e[36m###\e[33m 👀 Verifying gitrepositories & kustomizations\e[39m"
 kubectl get -n $NAMESPACE gitrepositories,kustomizations -A
